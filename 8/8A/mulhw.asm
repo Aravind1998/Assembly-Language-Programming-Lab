76fcb107a77582c6dafdacd96f519232e470da17
Aravind1998 <aravindsuresh98@gmail.com>
2017-03-19 22:01:09 +0530

    Create mulhw.asm
    This is an hardware program which is used to multiply two 8 bit numbers x and y.

---
+++ 8/8A/mulhw.asm
@@ -0,0 +1,18 @@
+.model small
+.code
+mov dx,9803h
+mov al,82h
+out dx,al
+mov dx,9801h
+in al,dx
+mov bl,al
+mov dx,9801h
+in al,dx
+mul bl
+mov dx,9800h
+out dx,al
+mov al,ah
+out dx,al
+mov ah,4ch
+int 21h
+end
